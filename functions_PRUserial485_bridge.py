411af1ca7cdd5f24259ca5ac20a6ef9be15e1a1e
Patricia Nallin <patricia.nallin@lnls.br>
2018-12-17 16:50:51 -0200

    minor changes

--- functions_PRUserial485_bridge.py
+++ functions_PRUserial485_bridge.py
@@ -5,9 +5,11 @@ if importlib.find_loader('siriuspy'):
 
 # FUNCAO PARA ENCONTRAR O IP A SE CONECTAR
 def find_BBB_IP():
+    return
 
 
 def control_PRUserial485():
     bbbname = socket.gethostname().replace('--', ':')
     bsmp_devs = PSSearch.conv_bbbname_2_psnames(bbbname)
     psnames, bsmp_ids = zip(*bsmp_devs)
+    return
