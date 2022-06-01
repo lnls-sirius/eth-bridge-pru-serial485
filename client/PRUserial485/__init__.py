ebc67480a2283ec574436ebffe245a2cbef9f618
gfrn <guilherme.freitas@cnpem.br>
2022-06-01 12:19:55 -0300

    Restores Makefile, version file, symlinks

--- client/PRUserial485/__init__.py
+++ client/PRUserial485/__init__.py
@@ -1,5 +1,7 @@
 """PRUserial485 client module for BBB ethernet bridge"""
 
+import os as _os
 from .PRUserial485 import *
 
-__version__ = "2.9.0"
+with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
+    __version__ = _f.read().strip()
